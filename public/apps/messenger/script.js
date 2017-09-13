0a052da425e67c882357ddaac52acfaa504d4bf6
Roy Varghese Mathew <vargs77@cs.washington.edu>
2017-09-12 17:57:55 -0700

    bare-bones local bi-directional VOIP

--- public/apps/messenger/script.js
+++ public/apps/messenger/script.js
@@ -1,115 +1,131 @@
 // Make connection
-/*global io*/
-var socket = io.connect('/');
+var socket = io.connect("/");
 
-var message = document.getElementById('message');
-var sendButton = document.getElementById('send');
-var chatBox = document.getElementById('chat-box');
+// localStorage.debug = '*';
+function socketEmit(emitType, data) {
+  console.log('Emmitting \'' + emitType + '\': ' + JSON.stringify(data));
+  socket.emit(emitType, data);
+}
 
-var offerOptions = {
-  offerToReceiveAudio: 1,
-  offerToReceiveVideo: 0,
-  voiceActivityDetection: false
-};
+function logSocketReceive(emitType, data) {
+  console.log("Received Emit '" + emitType + "': " + JSON.stringify(data));
+}
 
+// Document elements which handle core messaging services
+var message = document.getElementById("message");
+var sendButton = document.getElementById("send");
+var chatBox = document.getElementById("chat-box");
+
+// Handle that the user gives itself
 var handle = undefined;
 
 function display(name, output, color) {
-  var newMsg = document.createElement('p');
-  newMsg.setAttribute('class', 'messages');
+  var newMsg = document.createElement("p");
+  newMsg.setAttribute("class", "messages");
 
-  if (color){
+  if (color) {
     newMsg.style.color = color;
   }
 
-  newMsg.innerHTML = '<p class=\'messages\'><b>' + name + '</b> ' + output;
+  newMsg.innerHTML = "<p class='messages'><b>" + name + "</b> " + output;
   chatBox.appendChild(newMsg);
 }
 
-function send(){
+function send() {
   var text = message.value.trim();
-  message.value = '';
+  message.value = "";
   if (handle == undefined) {
     return;
   }
-  if (text === ''){
+  if (text === "") {
     return;
   }
 
-  if (text.charAt(0) == '/') {
+  if (text.charAt(0) == "/") {
     var parts = parseCommand(text);
-    socket.emit('command', {
-        message: text,
-        parts: parts
+    socketEmit("command", {
+      message: text,
+      parts: parts
     });
   }
 
-  socket.emit('chat', {
-        message: text,
-        handle: handle
+  socketEmit("chat", {
+    message: text,
+    handle: handle
   });
 }
 
-sendButton.addEventListener('click', send);
-message.addEventListener('keyup', function(event){
+sendButton.addEventListener("click", send);
+message.addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     send();
   }
-})
+});
 
 // Parse Commands
-function parseCommand(rawText){
+function parseCommand(rawText) {
   var parsingRegex = /(^\/\S*)(?:[^\n\r\S]+(\S*))?(?:[^\n\r\S]+(.*))?/;
   var regexSubgroups = parsingRegex.exec(rawText);
   if (!regexSubgroups) {
     return null;
   }
 
-  var parts = {}
+  var parts = {};
   var i = 1;
-  for (; i < regexSubgroups.length && regexSubgroups[i]; i++){
+  for (; i < regexSubgroups.length && regexSubgroups[i]; i++) {
     parts[i - 1] = regexSubgroups[i];
   }
-  parts['size'] = i - 1;
-  for (; i < 3; i++){
+  parts["size"] = i - 1;
+  for (; i < 3; i++) {
     parts[i - 1] = undefined;
   }
   return parts;
 }
 
-
 // Receiving Event Logic
-socket.on('welcome', function(data) {
+socket.on("welcome", function(data) {
+  logSocketReceive("Welcome", data);
+
   handle = data.newName;
-  display('Console:', data.message);
+  display("Console:", data.message);
 });
 
-socket.on('chat', function(data){
-    display(data.handle, data.message);
+socket.on("chat", function(data) {
+  logSocketReceive("chat", data);
+
+  display(data.handle, data.message);
 });
 
-socket.on('console-message', function(data) {
-  display('Console', data.message);
+socket.on("console-message", function(data) {
+  logSocketReceive("console-message", data);
+
+  display("Console", data.message);
 });
 
-socket.on('change-name', function(data){
+socket.on("change-name", function(data) {
+  logSocketReceive("change-name", data);
+
   if (data.result === true) {
-    display('Console:', 'Your name is now ' + data.newName);
+    display("Console:", "Your name is now " + data.newName);
     handle = data.newName;
   } else {
-    display('Console:', 'Can\'t pick that name');
+    display("Console:", "Can't pick that name");
   }
 });
 
-socket.on('list', function (data){
-  var elements = '';
+socket.on("list", function(data) {
+  logSocketReceive("list", data);
+
+  var elements = "";
   for (var prop in data) {
-    elements += prop + '<br/>';
+    elements += prop + "<br/>";
   }
-  display('', elements);
+  display("", elements);
 });
 
-socket.on('messenger-error', function (data){
+socket.on("messenger-error", function(data) {
+  logSocketReceive("messenger-error", data);
+
   var errorMsg = data.message;
-  display('Error:', errorMsg, 'red');
+  display("Error:", errorMsg, "red");
 });
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-// Dependencies
-var express = require('express');
-var http = require('http');
-var messageServer = require('./lib/messenger-server');
-var fs = require('fs');
+// Module Dependencies
+var express = require("express");
+var http = require("http");
+var messageServer = require("./lib/messenger-server");
+var fs = require("fs");
 
 // Initial SetUp Data
-var setupData = JSON.parse(
-  fs.readFileSync('setup-data.json'), 'utf8');
+var setupData = JSON.parse(fs.readFileSync("setup-data.json"), "utf8");
 
 // Set Up HTTP and Sockets.io Server
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 var server = http.createServer(app);
-server.listen(setupData.port.toString(), function(){
-  console.log('Server is listening on port ' + setupData.port);
+server.listen(setupData.port.toString(), function() {
+  console.log("Server is listening on port " + setupData.port);
 });
 
 var io = messageServer(server);
