8c05499e162375d562e69f2703044fe58763edd9
Roy Varghese Mathew <vargs77@cs.washington.edu>
2017-11-15 17:17:22 -0800

    adds homepage

---
+++ public/apps/homepage/script.js
@@ -0,0 +1,127 @@
+var data;
+
+// Gets JSON data from address addr and loads it into obj
+function loadJSON(addr) { 
+	var jsonData;
+
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType('application/json');
+	xobj.open('GET', addr, false);
+	xobj.onreadystatechange = function () {
+				if (xobj.readyState == 4 && xobj.status == '200') {
+					jsonData = JSON.parse(xobj.responseText);
+				}
+	};
+	xobj.send(null);
+	return jsonData;
+}
+
+var addr = 'data.json';
+
+data = loadJSON(addr);
+data = data.data;
+
+// code taken from https://stackoverflow.com/questions/10240110/how-do-you-cache-an-image-in-javascript
+function preloadImages(array) {
+  if (!preloadImages.list) {
+      preloadImages.list = [];
+  }
+  var list = preloadImages.list;
+  for (var i = 0; i < array.length; i++) {
+      var img = new Image();
+      img.onload = function() {
+          var index = list.indexOf(this);
+          if (index !== -1) {
+              // remove image from the array once it's loaded
+              // for memory consumption reasons
+              list.splice(index, 1);
+          }
+      }
+      list.push(img);
+      img.src = array[i].url;
+  }
+}
+preloadImages(data);
+
+
+
+
+
+function setBackground(idx) {
+  var imageURL = data[idx].url;
+  console.log(imageURL);
+  document.body.style.background = "url('" + imageURL + "') no-repeat center center fixed";
+  document.body.style.backgroundSize = screen.width + 'px';
+}
+var backgroundIdx = 1;
+if (document.cookie) {
+  var cookie = document.cookie.split(';');
+  var idx = (cookie[0].split('='))[1];
+  backgroundIdx = parseInt(idx);
+}
+setBackground(backgroundIdx);
+function updateCookie() {
+  var newDate = new Date();
+  newDate = new Date(newDate.getFullYear() + 1, newDate.getMonth(), newDate.getDate());
+  document.cookie = "bidx=" + backgroundIdx + ";expires=" + newDate.toUTCString();
+}
+updateCookie();
+
+function prevBackground() {
+  if (backgroundIdx > 1) {
+    backgroundIdx--;
+    setBackground(backgroundIdx);
+    updateCookie();
+  }
+}
+
+function nextBackground() {
+  if (backgroundIdx < data.length - 1) {
+    backgroundIdx++;
+    setBackground(backgroundIdx);
+    updateCookie();
+  }
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  if (h > 12) {h -= 12;}
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+var input = document.getElementById('search');
+input.addEventListener('keypress', (e) => {
+  if(e.keyCode==13) {
+    googleSearch();
+  }
+})
+
+function googleSearch() {
+  input = document.getElementById('search');
+  var searchText = input.value;
+  if (searchText.length == 0) {
+    return;
+  } else {
+    searchText = searchText.split(' ');
+  }
+  var searchURL = 'https://www.google.com/search?q=';
+  for (var i = 0; i < searchText.length; i++) {
+    searchURL += searchText[i] + '+';
+  }
+
+
+  window.location.href = searchURL;
+}
+
+input.value = '';
--- server.js
+++ server.js
@@ -1,17 +1,66 @@
 // Module Dependencies
 var express = require("express");
+var request = require("request");
 var http = require("http");
 var https = require('https');
 var messageServer = require("./lib/messenger-server");
 var fs = require("fs");
 
-// Encryption Cert and Key
-var privateKey  = fs.readFileSync('/etc/letsencrypt/live/royvmathew.com/privkey.pem');
-var certificate = fs.readFileSync('/etc/letsencrypt/live/royvmathew.com/fullchain.pem');
-var credentials = {key: privateKey, cert: certificate};
+var earthTopData;
+var imageIdx = 0;
+var imageDataJSON = {};
+imageDataJSON.data = [];
+var tmpCounter = 0;
+var fullCount;
+
+// Code taken from https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+var download = function(uri, idx, data){
+    request.head(uri, function(err, res, body){
+      console.log('content-type:', res.headers['content-type']);
+      if (idx === undefined || idx === null) {
+        request(uri, loadJSON);
+      } else {
+        if (res.headers['content-type'] === 'image/jpeg') {
+            var tmpIdx = imageIdx;
+            imageIdx++;
+          request(uri).pipe(fs.createWriteStream('public/apps/homepage/images/' + imageIdx + '.png')).on('close', () => {
+            imageDataJSON.data[tmpIdx] = {};
+            imageDataJSON.data[tmpIdx]["url"] = 'images/' + tmpIdx + '.png';
+            imageDataJSON.data[tmpIdx]["permalink"] = earthTopData.data.children[idx].data.permalink;
+            tmpCounter++;
+            if (tmpCounter == fullCount) {
+                earthTopData = null;
+                fs.writeFile('public/apps/homepage/data.json', JSON.stringify(imageDataJSON), () => {
+                    earthTopData = null;
+                    imageDataJSON.data = [];
+                    tmpCounter = 0;
+                });
+            }
+          });
+        } else {
+            tmpCounter++;
+        }
+      }
+    });
+};
+
+function loadJSON(e, res, body) {
+    earthTopData = JSON.parse(body);
+    fullCount = earthTopData.data.children.length;
+    for (var i = 0; i < fullCount; i++) {
+        download(earthTopData.data.children[i].data.url, i, earthTopData);
+    }
+}
+
+var addr = 'https://www.reddit.com/r/EarthPorn/top/.json';
 
 // Initial SetUp Data
 var setupData = JSON.parse(fs.readFileSync("setup-data.json"), "utf8");
+
+// Encryption Cert and Key
+var privateKey  = fs.readFileSync('../ssl/privkey.pem');
+var certificate = fs.readFileSync('../ssl/fullchain.pem');
+var credentials = {key: privateKey, cert: certificate};
 
 // Set Up HTTP redirect to HTTPS
 var app = express();
@@ -26,3 +75,10 @@ var httpsServer = https.createServer(credentials, app).listen(443);
 
 messageServer(httpsServer);
 console.log('End of Server.js');
+
+earthTopData = download(addr);
+var tmp = setInterval(() => {
+    earthTopData = null;
+    earthTopData = download(addr);
+}, 1000 * 60 * 60);
+
